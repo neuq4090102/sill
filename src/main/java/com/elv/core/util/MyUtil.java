842b9bae44695389c8f9d92ffcd2c258fd29dce9
neuq4090102 <neuq4090102@126.com>
2020-05-29 12:37:51 +0800

    init

---
+++ src/main/java/com/elv/core/util/MyUtil.java
@@ -0,0 +1,11 @@
+package com.elv.core.util;
+
+/**
+ * @author lxh
+ * @date 2020-05-29
+ */
+public class MyUtil {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
